7ed11c3fd2f8bdc3a4ad643bea6ad9908f1de1e4
Fumitoshi Ukai <fumitoshi.ukai@gmail.com>
2016-01-13 23:17:35 +0900

    chapter1 trace: Add unit tests (2)

--- chapter1/trace/tracer_test.go
+++ chapter1/trace/tracer_test.go
@@ -1,9 +1,19 @@
 package trace
 
 import (
+	"bytes"
 	"testing"
 )
 
 func TestNew(t *testing.T) {
-	t.Error("まだテストを作成していません")
+	var buf bytes.Buffer
+	tracer := New(&buf)
+	if tracer == nil {
+		t.Error("Newからの戻り値がnilです")
+	} else {
+		tracer.Trace("こんにちは、traceパッケージ")
+		if buf.String() != "こんにちは、traceパッケージ\n" {
+			t.Errorf("'%s'という誤った文字列が出力されました", buf.String())
+		}
+	}
 }
