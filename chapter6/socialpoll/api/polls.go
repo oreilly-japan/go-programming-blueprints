0e7f839033233a3cde7b4f3f70872a3656558d08
Fumitoshi Ukai <fumitoshi.ukai@gmail.com>
2016-01-14 00:47:40 +0900

    chapter6 api: Add poll struct

---
+++ chapter6/socialpoll/api/polls.go
@@ -0,0 +1,10 @@
+package main
+
+import "gopkg.in/mgo.v2/bson"
+
+type poll struct {
+	ID      bson.ObjectId  `bson:"_id" json:"id"`
+	Title   string         `json:"title"`
+	Options []string       `json:"options"`
+	Results map[string]int `json:"results,omitempty"`
+}
