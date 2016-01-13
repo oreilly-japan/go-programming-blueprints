19338a2a46b643ace02df082ed0d7e6bfab5fad0
Fumitoshi Ukai <fumitoshi.ukai@gmail.com>
2016-01-14 01:19:14 +0900

    chapter8 backupd: Add the backup tool daemon

---
+++ chapter8/backup/cmds/backupd/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"flag"
+	"log"
+
+	"github.com/matryer/filedb"
+	"github.com/oreilly-japan/go-programming-blueprints/chapter8/backup"
+)
+
+func main() {
+	var fatalErr error
+	defer func() {
+		if fatalErr != nil {
+			log.Fatalln(fatalErr)
+		}
+	}()
+	var (
+		interval = flag.Int("interval", 10, "チェックの間隔(秒単位)")
+		archive  = flag.String("archive", "archive", "アーカイブの保存先")
+		dbpath   = flag.String("db", "./db", "filedbデータベースへのパス")
+	)
+	flag.Parse()
+
+	m := &backup.Monitor{
+		Destination: *archive,
+		Archiver:    backup.ZIP,
+		Paths:       make(map[string]string),
+	}
+
+	db, err := filedb.Dial(*dbpath)
+	if err != nil {
+		fatalErr = err
+		return
+	}
+	defer db.Close()
+	col, err := db.C("paths")
+	if err != nil {
+		fatalErr = err
+		return
+	}
+
+}
