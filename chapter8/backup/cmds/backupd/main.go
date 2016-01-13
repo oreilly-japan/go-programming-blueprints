0018bd412a8ca8643b7ad42c168a420ba961ae46
Fumitoshi Ukai <fumitoshi.ukai@gmail.com>
2016-01-14 01:22:26 +0900

    chapter8 backupd: Update filedb records

--- chapter8/backup/cmds/backupd/main.go
+++ chapter8/backup/cmds/backupd/main.go
@@ -85,3 +85,33 @@ Loop:
 	}
 
 }
+
+func check(m *backup.Monitor, col *filedb.C) {
+	log.Println("チェックします...")
+	counter, err := m.Now()
+	if err != nil {
+		log.Panicln("バックアップに失敗しました:", err)
+	}
+	if counter > 0 {
+		log.Printf(" %d個のディレクトリをアーカイブしました\n", counter)
+		// ハッシュ値を更新します
+		var path path
+		col.SelectEach(func(_ int, data []byte) (bool, []byte, bool) {
+			if err := json.Unmarshal(data, &path); err != nil {
+				log.Println("JSONデータの読み込みに失敗しました。"+
+					"次の項目に進みます:", err)
+				return true, data, false
+			}
+			path.Hash, _ = m.Paths[path.Path]
+			newdata, err := json.Marshal(&path)
+			if err != nil {
+				log.Println("JSONデータの書き出しに失敗しました。"+
+					"次の項目に進みます:", err)
+				return true, data, false
+			}
+			return true, newdata, false
+		})
+	} else {
+		log.Println(" 変更はありません")
+	}
+}
