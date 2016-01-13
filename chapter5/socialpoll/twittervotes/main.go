9ee1ad655fcde0db33f72c2c1244a2497a0f55fb
Fumitoshi Ukai <fumitoshi.ukai@gmail.com>
2016-01-14 00:31:12 +0900

    chapter5 twittervotes: Read from MongoDB

--- chapter5/socialpoll/twittervotes/main.go
+++ chapter5/socialpoll/twittervotes/main.go
@@ -1,3 +1,37 @@
 package main
 
+import (
+	"log"
+
+	"gopkg.in/mgo.v2"
+)
+
+var db *mgo.Session
+
+func dialdb() error {
+	var err error
+	log.Println("MongoDBにダイヤル中: localhost")
+	db, err = mgo.Dial("localhost")
+	return err
+}
+func closedb() {
+	db.Close()
+	log.Println("データベース接続が閉じられました")
+}
+
+type poll struct {
+	Options []string
+}
+
+func loadOptions() ([]string, error) {
+	var options []string
+	iter := db.DB("ballots").C("polls").Find(nil).Iter()
+	var p poll
+	for iter.Next(&p) {
+		options = append(options, p.Options...)
+	}
+	iter.Close()
+	return options, iter.Err()
+}
+
 func main() {}
