083669d1fc43dd7329d59fb8e54d0cd15b44886a
Fumitoshi Ukai <fumitoshi.ukai@gmail.com>
2016-01-14 01:13:05 +0900

    chapter8 backup: Add the user command-line tool

---
+++ chapter8/backup/cmds/backup/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"log"
+)
+
+func main() {
+	var fatalErr error
+	defer func() {
+		if fatalErr != nil {
+			flag.PrintDefaults()
+			log.Fatalln(fatalErr)
+		}
+	}()
+	var (
+		dbpath = flag.String("db", "./backupdata", "データベースのディレクトリへのパス")
+	)
+	flag.Parse()
+	args := flag.Args()
+	if len(args) < 1 {
+		fatalErr = errors.New("エラー; コマンドを指定してください")
+		return
+	}
+}
