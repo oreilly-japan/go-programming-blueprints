ef9df3f64b2f6e2fd4165b5e934a3d14adc09faa
Fumitoshi Ukai <fumitoshi.ukai@gmail.com>
2016-01-14 00:50:08 +0900

    chapter6 api: Create a poll

--- chapter6/socialpoll/api/polls.go
+++ chapter6/socialpoll/api/polls.go
@@ -52,8 +52,22 @@ func handlePollsGet(w http.ResponseWriter, r *http.Request) {
 }
 
 func handlePollsPost(w http.ResponseWriter, r *http.Request) {
-	respondErr(w, r, http.StatusInternalServerError, errors.New("未実装です"))
+	db := GetVar(r, "db").(*mgo.Database)
+	c := db.C("polls")
+	var p poll
+	if err := decodeBody(r, &p); err != nil {
+		respondErr(w, r, http.StatusBadRequest, "リクエストから調査項目を読み込めません", err)
+		return
+	}
+	p.ID = bson.NewObjectId()
+	if err := c.Insert(p); err != nil {
+		respondErr(w, r, http.StatusInternalServerError, "調査項目の格納に失敗しました", err)
+		return
+	}
+	w.Header().Set("Location", "polls/"+p.ID.Hex())
+	respond(w, r, http.StatusCreated, nil)
 }
+
 func handlePollsDelete(w http.ResponseWriter, r *http.Request) {
 	respondErr(w, r, http.StatusInternalServerError, errors.New("未実装です"))
 }
