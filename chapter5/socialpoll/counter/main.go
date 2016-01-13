f87c5f083a2d67ebc1b2c9885712cd240012e0af
Fumitoshi Ukai <fumitoshi.ukai@gmail.com>
2016-01-14 00:36:46 +0900

    chapter5 counter: Connect to the database

--- chapter5/socialpoll/counter/main.go
+++ chapter5/socialpoll/counter/main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"os"
+
+	"gopkg.in/mgo.v2"
 )
 
 var fatalErr error
@@ -19,4 +22,16 @@ func main() {
 			os.Exit(1)
 		}
 	}()
+	log.Println("データベースに接続します...")
+	db, err := mgo.Dial("localhost")
+	if err != nil {
+		fatal(err)
+		return
+	}
+	defer func() {
+		log.Println("データベース接続を閉じます...")
+		db.Close()
+	}()
+	pollData := db.DB("ballots").C("polls")
+
 }
