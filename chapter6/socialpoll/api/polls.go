2a1feeccf45bbfd0e7ec5b160a46980a52cd3ea9
Fumitoshi Ukai <fumitoshi.ukai@gmail.com>
2016-01-14 00:48:58 +0900

    chapter6 api: Handle endpoints

--- chapter6/socialpoll/api/polls.go
+++ chapter6/socialpoll/api/polls.go
@@ -1,10 +1,41 @@
 package main
 
-import "gopkg.in/mgo.v2/bson"
+import (
+	"errors"
+	"net/http"
+
+	"gopkg.in/mgo.v2/bson"
+)
 
 type poll struct {
 	ID      bson.ObjectId  `bson:"_id" json:"id"`
 	Title   string         `json:"title"`
 	Options []string       `json:"options"`
 	Results map[string]int `json:"results,omitempty"`
+}
+
+func handlePolls(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		handlePollsGet(w, r)
+		return
+	case "POST":
+		handlePollsPost(w, r)
+		return
+	case "DELETE":
+		handlePollsDelete(w, r)
+		return
+	}
+	// 未対応のHTTPメソッド
+	respondHTTPErr(w, r, http.StatusNotFound)
+}
+
+func handlePollsGet(w http.ResponseWriter, r *http.Request) {
+	respondErr(w, r, http.StatusInternalServerError, errors.New("未実装です"))
+}
+func handlePollsPost(w http.ResponseWriter, r *http.Request) {
+	respondErr(w, r, http.StatusInternalServerError, errors.New("未実装です"))
+}
+func handlePollsDelete(w http.ResponseWriter, r *http.Request) {
+	respondErr(w, r, http.StatusInternalServerError, errors.New("未実装です"))
 }
