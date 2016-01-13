c30aa37de9d49803e83d46edce55b28815b929ae
Fumitoshi Ukai <fumitoshi.ukai@gmail.com>
2016-01-13 23:11:14 +0900

    chapter1 chat: Use helper functions to remove complexity

--- chapter1/chat/room.go
+++ chapter1/chat/room.go
@@ -18,6 +18,16 @@ type room struct {
 	clients map[*client]bool
 }
 
+// newRoomはすぐに利用できるチャットルームを生成して返します。
+func newRoom() *room {
+	return &room{
+		forward: make(chan []byte),
+		join:    make(chan *client),
+		leave:   make(chan *client),
+		clients: make(map[*client]bool),
+	}
+}
+
 func (r *room) run() {
 	for {
 		select {
