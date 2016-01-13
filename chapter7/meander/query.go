1a9d25c3cda1a826d8a498c22d66bd6ec50e7033
Fumitoshi Ukai <fumitoshi.ukai@gmail.com>
2016-01-14 00:59:48 +0900

    chapter7 meander: Define place type

---
+++ chapter7/meander/query.go
@@ -0,0 +1,34 @@
+package meander
+
+type Place struct {
+	*googleGeometry `json:"geometry"`
+	Name            string         `json:"name"`
+	Icon            string         `json:"icon"`
+	Photos          []*googlePhoto `json:"photos"`
+	Vicinity        string         `json:"vicinity"`
+}
+type googleResponse struct {
+	Results []*Place `json:"results"`
+}
+type googleGeometry struct {
+	*googleLocation `json:"location"`
+}
+type googleLocation struct {
+	Lat float64 `json:"lat"`
+	Lng float64 `json:"lng"`
+}
+type googlePhoto struct {
+	PhotoRef string `json:"photo_reference"`
+	URL      string `json:"url"`
+}
+
+func (p *Place) Public() interface{} {
+	return map[string]interface{}{
+		"name":     p.Name,
+		"icon":     p.Icon,
+		"photos":   p.Photos,
+		"vicinity": p.Vicinity,
+		"lat":      p.Lat,
+		"lng":      p.Lng,
+	}
+}
