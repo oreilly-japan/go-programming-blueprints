94e44e2738304372dde96148291e9d3507571fa2
Fumitoshi Ukai <fumitoshi.ukai@gmail.com>
2016-01-13 23:54:32 +0900

    chapter3 chat: Implement logging out

--- chapter3/chat/auth.go
+++ chapter3/chat/auth.go
@@ -15,7 +15,7 @@ type authHandler struct {
 }
 
 func (h *authHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if _, err := r.Cookie("auth"); err == http.ErrNoCookie {
+	if cookie, err := r.Cookie("auth"); err == http.ErrNoCookie || cookie.Value == "" {
 		// 未認証
 		w.Header().Set("Location", "/login")
 		w.WriteHeader(http.StatusTemporaryRedirect)
--- chapter3/chat/main.go
+++ chapter3/chat/main.go
@@ -56,6 +56,17 @@ func main() {
 	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
 	http.Handle("/login", &templateHandler{filename: "login.html"})
 	http.HandleFunc("/auth/", loginHandler)
+	http.HandleFunc("/logout", func(w http.ResponseWriter, r *http.Request) {
+		http.SetCookie(w, &http.Cookie{
+			Name:   "auth",
+			Value:  "",
+			Path:   "/",
+			MaxAge: -1,
+		})
+		w.Header()["Location"] = []string{"/chat"}
+		w.WriteHeader(http.StatusTemporaryRedirect)
+	})
+
 	http.Handle("/room", r)
 	// チャットルームを開始します
 	go r.run()
