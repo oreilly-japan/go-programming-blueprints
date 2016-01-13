752851055134f32244cb24d4ccdf251e443e7d07
Fumitoshi Ukai <fumitoshi.ukai@gmail.com>
2016-01-14 00:26:30 +0900

    chapter4 available

---
+++ chapter4/available/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"strings"
+	"time"
+)
+
+func exists(domain string) (bool, error) {
+	const whoisServer string = "com.whois-servers.net"
+	conn, err := net.Dial("tcp", whoisServer+":43")
+	if err != nil {
+		return false, err
+	}
+	defer conn.Close()
+	conn.Write([]byte(domain + "\r\n"))
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		if strings.Contains(strings.ToLower(scanner.Text()), "no match") {
+			return false, nil
+		}
+	}
+	return true, nil
+}
+
+var marks = map[bool]string{true: "○", false: "×"}
+
+func main() {
+	s := bufio.NewScanner(os.Stdin)
+	for s.Scan() {
+		domain := s.Text()
+		fmt.Print(domain, " ")
+		exist, err := exists(domain)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		fmt.Println(marks[!exist])
+		time.Sleep(1 * time.Second)
+	}
+}
