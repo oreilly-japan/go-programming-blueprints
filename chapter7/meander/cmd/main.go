4e7c4aed954dec2b094bdb330de3bb0af026af7a
Fumitoshi Ukai <fumitoshi.ukai@gmail.com>
2016-01-14 00:58:41 +0900

    chapter7 meander: Add public views of Go structs

--- chapter7/meander/cmd/main.go
+++ chapter7/meander/cmd/main.go
@@ -17,5 +17,9 @@ func main() {
 	http.ListenAndServe(":8080", http.DefaultServeMux)
 }
 func respond(w http.ResponseWriter, r *http.Request, data []interface{}) error {
-	return json.NewEncoder(w).Encode(data)
+	publicData := make([]interface{}, len(data))
+	for i, d := range data {
+		publicData[i] = meander.Public(d)
+	}
+	return json.NewEncoder(w).Encode(publicData)
 }
--- chapter7/meander/journeys.go
+++ chapter7/meander/journeys.go
@@ -1,5 +1,7 @@
 package meander
 
+import "strings"
+
 type j struct {
 	Name       string
 	PlaceTypes []string
@@ -16,4 +18,11 @@ var Journeys = []interface{}{
 		"cemetery", "library", "art_gallery"}},
 	&j{Name: "リラックス", PlaceTypes: []string{"hair_care",
 		"beauty_salon", "cafe", "spa"}},
+}
+
+func (j *j) Public() interface{} {
+	return map[string]interface{}{
+		"name":    j.Name,
+		"journey": strings.Join(j.PlaceTypes, "|"),
+	}
 }
