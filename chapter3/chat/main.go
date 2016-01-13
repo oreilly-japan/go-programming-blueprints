5943783afcc54521603cf9d2e1f0b2d251660141
Fumitoshi Ukai <fumitoshi.ukai@gmail.com>
2016-01-14 00:05:11 +0900

    chapter3 chat: Add an upload form

--- chapter3/chat/main.go
+++ chapter3/chat/main.go
@@ -66,7 +66,7 @@ func main() {
 		w.Header()["Location"] = []string{"/chat"}
 		w.WriteHeader(http.StatusTemporaryRedirect)
 	})
-
+	http.Handle("/upload", &templateHandler{filename: "upload.html"})
 	http.Handle("/room", r)
 	// チャットルームを開始します
 	go r.run()
