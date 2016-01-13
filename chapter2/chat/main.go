4c78a1e0d99ab855ec7ca61f0afaa5f0467ed5bb
Fumitoshi Ukai <fumitoshi.ukai@gmail.com>
2016-01-13 23:36:48 +0900

    chapter2 chat: Present the user data

--- chapter2/chat/main.go
+++ chapter2/chat/main.go
@@ -14,6 +14,7 @@ import (
 	"github.com/stretchr/gomniauth/providers/facebook"
 	"github.com/stretchr/gomniauth/providers/github"
 	"github.com/stretchr/gomniauth/providers/google"
+	"github.com/stretchr/objx"
 )
 
 // templは1つのテンプレートを表します
@@ -30,7 +31,13 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 			template.Must(template.ParseFiles(filepath.Join("templates",
 				t.filename)))
 	})
-	t.templ.Execute(w, r)
+	data := map[string]interface{}{
+		"Host": r.Host,
+	}
+	if authCookie, err := r.Cookie("auth"); err == nil {
+		data["UserData"] = objx.MustFromBase64(authCookie.Value)
+	}
+	t.templ.Execute(w, data)
 }
 
 func main() {
