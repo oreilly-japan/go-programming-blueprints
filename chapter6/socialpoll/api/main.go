d71f025d0d87a8ba6fa3de76f9768c6d3b97ec08
Fumitoshi Ukai <fumitoshi.ukai@gmail.com>
2016-01-14 00:42:34 +0900

    chapter6 api: Manage database session

--- chapter6/socialpoll/api/main.go
+++ chapter6/socialpoll/api/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"gopkg.in/mgo.v2"
+)
 
 func main() {}
 
@@ -16,4 +20,13 @@ func withAPIKey(fn http.HandlerFunc) http.HandlerFunc {
 
 func isValidAPIKey(key string) bool {
 	return key == "abc123"
+}
+
+func withData(d *mgo.Session, f http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		thisDb := d.Copy()
+		defer thisDb.Close()
+		SetVar(r, "db", thisDb.DB("ballots"))
+		f(w, r)
+	}
 }
