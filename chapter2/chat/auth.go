376049bce7f923ae9376f695bbf53640e49b042e
Fumitoshi Ukai <fumitoshi.ukai@gmail.com>
2016-01-13 23:30:53 +0900

    chapter2 chat: add login handler

--- chapter2/chat/auth.go
+++ chapter2/chat/auth.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"strings"
 )
 
 type authHandler struct {
@@ -23,4 +26,19 @@ func (h *authHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 func MustAuth(handler http.Handler) http.Handler {
 	return &authHandler{next: handler}
+}
+
+// loginHandlerはサードパーティーへのログインの処理を受け持ちます。
+// パスの形式: /auth/{action}/{provider}
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	segs := strings.Split(r.URL.Path, "/")
+	action := segs[2]
+	provider := segs[3]
+	switch action {
+	case "login":
+		log.Println("TODO: ログイン処理", provider)
+	default:
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "アクション%sには非対応です", action)
+	}
 }
--- chapter2/chat/main.go
+++ chapter2/chat/main.go
@@ -36,6 +36,7 @@ func main() {
 	r.tracer = trace.New(os.Stdout)
 	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
 	http.Handle("/login", &templateHandler{filename: "login.html"})
+	http.HandleFunc("/auth/", loginHandler)
 	http.Handle("/room", r)
 	// チャットルームを開始します
 	go r.run()
