2db8ccfa0c6494991278b49435bcac914a161840
Fumitoshi Ukai <fumitoshi.ukai@gmail.com>
2016-01-14 01:04:25 +0900

    chapter7 meander: Test-driven enumerator (4)

--- chapter7/meander/cost_level.go
+++ chapter7/meander/cost_level.go
@@ -1,5 +1,7 @@
 package meander
 
+import "strings"
+
 type Cost int8
 
 const (
@@ -30,4 +32,20 @@ func (l Cost) String() string {
 
 func ParseCost(s string) Cost {
 	return costStrings[s]
+}
+
+type CostRange struct {
+	From Cost
+	To   Cost
+}
+
+func (r CostRange) String() string {
+	return r.From.String() + "..." + r.To.String()
+}
+func ParseCostRange(s string) *CostRange {
+	segs := strings.Split(s, "...")
+	return &CostRange{
+		From: ParseCost(segs[0]),
+		To:   ParseCost(segs[1]),
+	}
 }
--- chapter7/meander/cost_level_test.go
+++ chapter7/meander/cost_level_test.go
@@ -33,3 +33,21 @@ func TestParseCost(t *testing.T) {
 	is.Equal(meander.Cost4, meander.ParseCost("$$$$"))
 	is.Equal(meander.Cost5, meander.ParseCost("$$$$$"))
 }
+
+func TestParseCostRange(t *testing.T) {
+	is := is.New(t)
+	var l *meander.CostRange
+	l = meander.ParseCostRange("$$...$$$")
+	is.Equal(l.From, meander.Cost2)
+	is.Equal(l.To, meander.Cost3)
+	l = meander.ParseCostRange("$...$$$$$")
+	is.Equal(l.From, meander.Cost1)
+	is.Equal(l.To, meander.Cost5)
+}
+func TestCostRangeString(t *testing.T) {
+	is := is.New(t)
+	is.Equal("$$...$$$$", (&meander.CostRange{
+		From: meander.Cost2,
+		To:   meander.Cost4,
+	}).String())
+}
