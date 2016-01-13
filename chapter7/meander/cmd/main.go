30dd918b8da25660cf5207a9c39fa1ff021eb6af
Fumitoshi Ukai <fumitoshi.ukai@gmail.com>
2016-01-14 01:06:20 +0900

    chapter7 meander: Add handlers that use query parameters

--- chapter7/meander/cmd/main.go
+++ chapter7/meander/cmd/main.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 	"os"
 	"runtime"
+	"strconv"
+	"strings"
 
 	"github.com/oreilly-japan/go-programming-blueprints/chapter7/meander"
 )
@@ -12,9 +14,22 @@ import (
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 	meander.APIKey = os.Getenv("GOOGLE_PLACES_API_KEY")
-	http.HandleFunc("/journeys", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/journeys", cors(func(w http.ResponseWriter, r *http.Request) {
 		respond(w, r, meander.Journeys)
-	})
+	}))
+	http.HandleFunc("/recommendations", cors(func(
+		w http.ResponseWriter, r *http.Request) {
+		q := &meander.Query{
+			Journey: strings.Split(r.URL.Query().Get("journey"), "|"),
+		}
+		q.Lat, _ = strconv.ParseFloat(r.URL.Query().Get("lat"), 64)
+		q.Lng, _ = strconv.ParseFloat(r.URL.Query().Get("lng"), 64)
+		q.Radius, _ = strconv.Atoi(r.URL.Query().Get("radius"))
+		q.CostRangeStr = r.URL.Query().Get("cost")
+		places := q.Run()
+		respond(w, r, places)
+	}))
+
 	http.ListenAndServe(":8080", http.DefaultServeMux)
 }
 func respond(w http.ResponseWriter, r *http.Request, data []interface{}) error {
@@ -23,4 +38,11 @@ func respond(w http.ResponseWriter, r *http.Request, data []interface{}) error {
 		publicData[i] = meander.Public(d)
 	}
 	return json.NewEncoder(w).Encode(publicData)
+}
+
+func cors(f http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		f(w, r)
+	}
 }
