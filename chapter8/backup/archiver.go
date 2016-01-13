4a5883a12b7b9b786aea35848b2fb635b511e666
Fumitoshi Ukai <fumitoshi.ukai@gmail.com>
2016-01-14 01:08:45 +0900

    chapter8 backup: Implement ZIP (2)

--- chapter8/backup/archiver.go
+++ chapter8/backup/archiver.go
@@ -1,5 +1,12 @@
 package backup
 
+import (
+	"archive/zip"
+	"io"
+	"os"
+	"path/filepath"
+)
+
 type Archiver interface {
 	Archive(src, dest string) error
 }
@@ -7,3 +14,36 @@ type Archiver interface {
 type zipper struct{}
 
 var ZIP Archiver = (*zipper)(nil)
+
+func (z *zipper) Archive(src, dest string) error {
+	if err := os.MkdirAll(filepath.Dir(dest), 0777); err != nil {
+		return err
+	}
+	out, err := os.Create(dest)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+	w := zip.NewWriter(out)
+	defer w.Close()
+	return filepath.Walk(src, func(path string, info os.FileInfo,
+		err error) error {
+		if info.IsDir() {
+			return nil // スキップします
+		}
+		if err != nil {
+			return err
+		}
+		in, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+		defer in.Close()
+		f, err := w.Create(path)
+		if err != nil {
+			return err
+		}
+		io.Copy(f, in)
+		return nil
+	})
+}
