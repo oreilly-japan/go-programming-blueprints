9188ddbeecaff236fc6efc1963fbf7eb9c1a8a81
Fumitoshi Ukai <fumitoshi.ukai@gmail.com>
2016-01-14 00:10:49 +0900

    chapter3 chat: Support different file types

--- chapter3/chat/avatar.go
+++ chapter3/chat/avatar.go
@@ -1,6 +1,10 @@
 package main
 
-import "errors"
+import (
+	"errors"
+	"io/ioutil"
+	"path/filepath"
+)
 
 // ErrNoAvatarはAvatarインスタンスがアバターのURLを返すことができない
 // 場合に発生するエラーです。
@@ -47,7 +51,16 @@ var UseFileSystemAvatar FileSystemAvatar
 func (_ FileSystemAvatar) GetAvatarURL(c *client) (string, error) {
 	if userid, ok := c.userData["userid"]; ok {
 		if useridStr, ok := userid.(string); ok {
-			return "/avatars/" + useridStr + ".jpg", nil
+			if files, err := ioutil.ReadDir("avatars"); err == nil {
+				for _, file := range files {
+					if file.IsDir() {
+						continue
+					}
+					if match, _ := filepath.Match(useridStr+"*", file.Name()); match {
+						return "/avatars/" + file.Name(), nil
+					}
+				}
+			}
 		}
 	}
 	return "", ErrNoAvatarURL
