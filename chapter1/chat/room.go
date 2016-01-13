aaae2256e0ea89df1754ad3c769af72915354e8b
Fumitoshi Ukai <fumitoshi.ukai@gmail.com>
2016-01-13 23:09:30 +0900

    chapter1 chat: Modeling a room

--- chapter1/chat/room.go
+++ chapter1/chat/room.go
@@ -3,4 +3,36 @@ package main
 type room struct {
 	// forwardは他のクライアントに転送するためのメッセージを保持するチャネルです。
 	forward chan []byte
+	// joinはチャットルームに参加しようとしているクライアントのためのチャネルです。
+	join chan *client
+	// leaveはチャットルームから退室しようとしているクライアントのためのチャネルです
+	leave chan *client
+	// clientsには在室しているすべてのクライアントが保持されます。
+	clients map[*client]bool
+}
+
+func (r *room) run() {
+	for {
+		select {
+		case client := <-r.join:
+			// 参加
+			r.clients[client] = true
+		case client := <-r.leave:
+			// 退室
+			delete(r.clients, client)
+			close(client.send)
+		case msg := <-r.forward:
+			// すべてのクライアントにメッセージを転送
+			for client := range r.clients {
+				select {
+				case client.send <- msg:
+					// メッセージを送信
+				default:
+					// 送信に失敗
+					delete(r.clients, client)
+					close(client.send)
+				}
+			}
+		}
+	}
 }
