31899e43398f07248c73fd01f1a73f9993ae2477
Fumitoshi Ukai <fumitoshi.ukai@gmail.com>
2016-01-13 23:38:46 +0900

    chapter2 chat: Argument messages with additional data

--- chapter2/chat/client.go
+++ chapter2/chat/client.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"time"
+
 	"github.com/gorilla/websocket"
 )
 
@@ -9,14 +11,19 @@ type client struct {
 	// socketはこのクライアントのためのWebSocketです。
 	socket *websocket.Conn
 	// sendはメッセージが送られるチャネルです。
-	send chan []byte
+	send chan *message
 	// roomはこのクライアントが参加しているチャットルームです。
 	room *room
+	// userDataはユーザーに関する情報を保持します
+	userData map[string]interface{}
 }
 
 func (c *client) read() {
 	for {
-		if _, msg, err := c.socket.ReadMessage(); err == nil {
+		var msg *message
+		if err := c.socket.ReadJSON(&msg); err == nil {
+			msg.When = time.Now()
+			msg.Name = c.userData["name"].(string)
 			c.room.forward <- msg
 		} else {
 			break
@@ -26,7 +33,7 @@ func (c *client) read() {
 }
 func (c *client) write() {
 	for msg := range c.send {
-		if err := c.socket.WriteMessage(websocket.TextMessage, msg); err != nil {
+		if err := c.socket.WriteJSON(msg); err != nil {
 			break
 		}
 	}
--- chapter2/chat/room.go
+++ chapter2/chat/room.go
@@ -6,11 +6,12 @@ import (
 
 	"github.com/gorilla/websocket"
 	"github.com/oreilly-japan/go-programming-blueprints/chapter2/trace"
+	"github.com/stretchr/objx"
 )
 
 type room struct {
 	// forwardは他のクライアントに転送するためのメッセージを保持するチャネルです。
-	forward chan []byte
+	forward chan *message
 	// joinはチャットルームに参加しようとしているクライアントのためのチャネルです。
 	join chan *client
 	// leaveはチャットルームから退室しようとしているクライアントのためのチャネルです
@@ -24,7 +25,7 @@ type room struct {
 // newRoomはすぐに利用できるチャットルームを生成して返します。
 func newRoom() *room {
 	return &room{
-		forward: make(chan []byte),
+		forward: make(chan *message),
 		join:    make(chan *client),
 		leave:   make(chan *client),
 		clients: make(map[*client]bool),
@@ -45,7 +46,7 @@ func (r *room) run() {
 			close(client.send)
 			r.tracer.Trace("クライアントが退室しました")
 		case msg := <-r.forward:
-			r.tracer.Trace("メッセージを受信しました: ", string(msg))
+			r.tracer.Trace("メッセージを受信しました: ", msg.Message)
 			// すべてのクライアントにメッセージを転送
 			for client := range r.clients {
 				select {
@@ -76,10 +77,16 @@ func (r *room) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 		log.Fatal("ServeHTTP:", err)
 		return
 	}
+	authCookie, err := req.Cookie("auth")
+	if err != nil {
+		log.Fatal("クッキーの取得に失敗しました:", err)
+		return
+	}
 	client := &client{
-		socket: socket,
-		send:   make(chan []byte, messageBufferSize),
-		room:   r,
+		socket:   socket,
+		send:     make(chan *message, messageBufferSize),
+		room:     r,
+		userData: objx.MustFromBase64(authCookie.Value),
 	}
 	r.join <- client
 	defer func() { r.leave <- client }()
