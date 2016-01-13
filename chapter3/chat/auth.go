8d767ef443a92a53b21512914e62796d19e08ac7
Fumitoshi Ukai <fumitoshi.ukai@gmail.com>
2016-01-13 23:50:46 +0900

    chapter3 chat: Get the avatar URL

--- chapter3/chat/auth.go
+++ chapter3/chat/auth.go
@@ -67,7 +67,8 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 		}
 
 		authCookieValue := objx.New(map[string]interface{}{
-			"name": user.Name(),
+			"name":       user.Name(),
+			"avatar_url": user.AvatarURL(),
 		}).MustBase64()
 		http.SetCookie(w, &http.Cookie{
 			Name:  "auth",
