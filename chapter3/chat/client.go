879a4afc0b70ae61017f640c4905136e670d8039
Fumitoshi Ukai <fumitoshi.ukai@gmail.com>
2016-01-14 00:00:11 +0900

    chapter3 chat: Use the auth avatar implementation

--- chapter3/chat/client.go
+++ chapter3/chat/client.go
@@ -24,9 +24,7 @@ func (c *client) read() {
 		if err := c.socket.ReadJSON(&msg); err == nil {
 			msg.When = time.Now()
 			msg.Name = c.userData["name"].(string)
-			if avatarURL, ok := c.userData["avatar_url"]; ok {
-				msg.AvatarURL = avatarURL.(string)
-			}
+			msg.AvatarURL, _ = c.room.avatar.GetAvatarURL(c)
 			c.room.forward <- msg
 		} else {
 			break
--- chapter3/chat/room.go
+++ chapter3/chat/room.go
@@ -20,16 +20,19 @@ type room struct {
 	clients map[*client]bool
 	// tracerはチャットルーム上で行われた操作のログを受け取ります。
 	tracer trace.Tracer
+	// avatarはアバターの情報を取得します。
+	avatar Avatar
 }
 
 // newRoomはすぐに利用できるチャットルームを生成して返します。
-func newRoom() *room {
+func newRoom(avatar Avatar) *room {
 	return &room{
 		forward: make(chan *message),
 		join:    make(chan *client),
 		leave:   make(chan *client),
 		clients: make(map[*client]bool),
 		tracer:  trace.Off(),
+		avatar:  avatar,
 	}
 }
 
