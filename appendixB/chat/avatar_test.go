9d21e9e38153f1996708fe2a48734e44acd3cc51
Fumitoshi Ukai <fumitoshi.ukai@gmail.com>
2016-01-14 01:24:41 +0900

    appendixB trace: Use struct instead of interface

---
+++ appendixB/chat/avatar_test.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+
+	gomniauthtest "github.com/stretchr/gomniauth/test"
+)
+
+func TestAuthAvatar(t *testing.T) {
+	var authAvatar AuthAvatar
+	testUser := &gomniauthtest.TestUser{}
+	testUser.On("AvatarURL").Return("", ErrNoAvatarURL)
+	testChatUser := &chatUser{User: testUser}
+	url, err := authAvatar.GetAvatarURL(testChatUser)
+	if err != ErrNoAvatarURL {
+		t.Error("値が存在しない場合、AuthAvatar.GetAvatarURLは" +
+			"ErrNoAvatarURLを返すべきです")
+	}
+	// 値をセットします
+	testUrl := "http://url-to-avatar/"
+	testUser = &gomniauthtest.TestUser{}
+	testChatUser.User = testUser
+	testUser.On("AvatarURL").Return(testUrl, nil)
+	url, err = authAvatar.GetAvatarURL(testChatUser)
+	if err != nil {
+		t.Error("値が存在する場合、AuthAvatar.GetAvatarURLは" +
+			"エラーを返すべきではありません")
+	} else {
+		if url != testUrl {
+			t.Error("AuthAvatar.GetAvatarURLは正しいURLを返すべきです")
+		}
+	}
+}
+
+func TestGravatarAvatar(t *testing.T) {
+	var gravatarAvatar GravatarAvatar
+	user := &chatUser{uniqueID: "abc"}
+	url, err := gravatarAvatar.GetAvatarURL(user)
+	if err != nil {
+		t.Error("GravatarAvitar.GetAvatarURLはエラーを返すべきではありません")
+	}
+	if url != "//www.gravatar.com/avatar/abc" {
+		t.Errorf("GravatarAvitar.GetAvatarURLが%sという誤った値を返しました", url)
+	}
+}
+
+func TestFileSystemAvatar(t *testing.T) {
+
+	// テスト用のアバターのファイルを生成します
+	filename := filepath.Join("avatars", "abc.jpg")
+	ioutil.WriteFile(filename, []byte{}, 0777)
+	defer func() { os.Remove(filename) }()
+
+	var fileSystemAvatar FileSystemAvatar
+	user := &chatUser{uniqueID: "abc"}
+	url, err := fileSystemAvatar.GetAvatarURL(user)
+	if err != nil {
+		t.Error("FileSystemAvatar.GetAvatarURLはエラーを返すべきではありません")
+	}
+	if url != "/avatars/abc.jpg" {
+		t.Errorf("FileSystemAvatar.GetAvatarURLが%sという誤った値を返しました", url)
+	}
+}
