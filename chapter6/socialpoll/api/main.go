ed207e48377de280148ab425440defd2541c01f8
Fumitoshi Ukai <fumitoshi.ukai@gmail.com>
2016-01-14 00:44:34 +0900

    chapter6 api: Support cross-browser resource sharing

--- chapter6/socialpoll/api/main.go
+++ chapter6/socialpoll/api/main.go
@@ -38,3 +38,11 @@ func withVars(fn http.HandlerFunc) http.HandlerFunc {
 		fn(w, r)
 	}
 }
+
+func withCORS(fn http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Expose-Headers", "Location")
+		fn(w, r)
+	}
+}
