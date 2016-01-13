731bc03d93dfcb15aaeb962ad04460d15ede4c71
Fumitoshi Ukai <fumitoshi.ukai@gmail.com>
2016-01-13 23:23:54 +0900

    chapter1 chat: Use our new trace package

--- chapter1/chat/main.go
+++ chapter1/chat/main.go
@@ -5,8 +5,11 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"os"
 	"path/filepath"
 	"sync"
+
+	"github.com/oreilly-japan/go-programming-blueprints/chapter1/trace"
 )
 
 // templは1つのテンプレートを表します
@@ -30,6 +33,7 @@ func main() {
 	var addr = flag.String("addr", ":8080", "アプリケーションのアドレス")
 	flag.Parse() // フラグを解釈します
 	r := newRoom()
+	r.tracer = trace.New(os.Stdout)
 	http.Handle("/", &templateHandler{filename: "chat.html"})
 	http.Handle("/room", r)
 	// チャットルームを開始します
--- chapter1/chat/room.go
+++ chapter1/chat/room.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/gorilla/websocket"
+	"github.com/oreilly-japan/go-programming-blueprints/chapter1/trace"
 )
 
 type room struct {
@@ -16,6 +17,8 @@ type room struct {
 	leave chan *client
 	// clientsには在室しているすべてのクライアントが保持されます。
 	clients map[*client]bool
+	// tracerはチャットルーム上で行われた操作のログを受け取ります。
+	tracer trace.Tracer
 }
 
 // newRoomはすぐに利用できるチャットルームを生成して返します。
@@ -34,20 +37,25 @@ func (r *room) run() {
 		case client := <-r.join:
 			// 参加
 			r.clients[client] = true
+			r.tracer.Trace("新しいクライアントが参加しました")
 		case client := <-r.leave:
 			// 退室
 			delete(r.clients, client)
 			close(client.send)
+			r.tracer.Trace("クライアントが退室しました")
 		case msg := <-r.forward:
+			r.tracer.Trace("メッセージを受信しました: ", string(msg))
 			// すべてのクライアントにメッセージを転送
 			for client := range r.clients {
 				select {
 				case client.send <- msg:
 					// メッセージを送信
+					r.tracer.Trace(" -- クライアントに送信されました")
 				default:
 					// 送信に失敗
 					delete(r.clients, client)
 					close(client.send)
+					r.tracer.Trace(" -- 送信に失敗しました。クライアントをクリーンアップします")
 				}
 			}
 		}
