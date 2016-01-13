a15bc87287314a198e50186c9b0c026d744034d0
Fumitoshi Ukai <fumitoshi.ukai@gmail.com>
2016-01-13 23:56:53 +0900

    chapter3 chat: Abstract the avatar URL process

---
+++ chapter3/chat/avatar.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"errors"
+)
+
+// ErrNoAvatarはAvatarインスタンスがアバターのURLを返すことができない
+// 場合に発生するエラーです。
+var ErrNoAvatarURL = errors.New("chat: アバターのURLを取得できません。")
+
+// Avatarはユーザーのプロフィール画像を表す型です。
+type Avatar interface {
+	// GetAvatarURLは指定されたクライアントのアバターのURLを返します。
+	// 問題が発生した場合にはエラーを返します。特に、URLを取得できなかった
+	// 場合にはErrNoAvatarURLを返します。
+	GetAvatarURL(c *client) (string, error)
+}
