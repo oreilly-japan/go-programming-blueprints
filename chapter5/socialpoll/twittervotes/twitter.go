4adf71b643368c440048cf610c3232dcb1c4b27e
Fumitoshi Ukai <fumitoshi.ukai@gmail.com>
2016-01-14 00:30:24 +0900

    chapter5 twittervotes: Read environment variables

--- chapter5/socialpoll/twittervotes/twitter.go
+++ chapter5/socialpoll/twittervotes/twitter.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"io"
+	"log"
 	"net"
+	"net/http"
+	"net/url"
+	"strconv"
+	"sync"
 	"time"
+
+	"github.com/garyburd/go-oauth/oauth"
+	"github.com/joeshaw/envdecode"
 )
 
 var conn net.Conn
@@ -30,4 +38,53 @@ func closeConn() {
 	if reader != nil {
 		reader.Close()
 	}
+}
+
+var (
+	authClient *oauth.Client
+	creds      *oauth.Credentials
+)
+
+func setupTwitterAuth() {
+	var ts struct {
+		ConsumerKey    string `env:"SP_TWITTER_KEY,required"`
+		ConsumerSecret string `env:"SP_TWITTER_SECRET,required"`
+		AccessToken    string `env:"SP_TWITTER_ACCESSTOKEN,required"`
+		AccessSecret   string `env:"SP_TWITTER_ACCESSSECRET,required"`
+	}
+	if err := envdecode.Decode(&ts); err != nil {
+		log.Fatalln(err)
+	}
+	creds = &oauth.Credentials{
+		Token:  ts.AccessToken,
+		Secret: ts.AccessSecret,
+	}
+	authClient = &oauth.Client{
+		Credentials: oauth.Credentials{
+			Token:  ts.ConsumerKey,
+			Secret: ts.ConsumerSecret,
+		},
+	}
+}
+
+var (
+	authSetupOnce sync.Once
+	httpClient    *http.Client
+)
+
+func makeRequest(req *http.Request, params url.Values) (*http.Response, error) {
+	authSetupOnce.Do(func() {
+		setupTwitterAuth()
+		httpClient = &http.Client{
+			Transport: &http.Transport{
+				Dial: dial,
+			},
+		}
+	})
+	formEnc := params.Encode()
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set("Content-Length", strconv.Itoa(len(formEnc)))
+	req.Header.Set("Authorization",
+		authClient.AuthorizationHeader(creds, "POST", req.URL, params))
+	return httpClient.Do(req)
 }
