5a7cca2b0352ee8356c3780d916b93cb46751d78
Fumitoshi Ukai <fumitoshi.ukai@gmail.com>
2016-01-13 23:21:47 +0900

    chapter1 trace: Implement the interface (2)

--- chapter1/trace/tracer.go
+++ chapter1/trace/tracer.go
@@ -1,6 +1,9 @@
 package trace
 
-import "io"
+import (
+	"fmt"
+	"io"
+)
 
 // Tracerはコード内での出来事を記録できるオブジェクトを表すインタフェースです。
 type Tracer interface {
@@ -15,4 +18,7 @@ type tracer struct {
 	out io.Writer
 }
 
-func (t *tracer) Trace(a ...interface{}) {}
+func (t *tracer) Trace(a ...interface{}) {
+	t.out.Write([]byte(fmt.Sprint(a...)))
+	t.out.Write([]byte("\n"))
+}
