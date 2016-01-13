ca9181c23c7d8ac259f44ac2b4e6087cf9fb322c
Fumitoshi Ukai <fumitoshi.ukai@gmail.com>
2016-01-14 00:32:45 +0900

    chapter5 twittervotes: Use signal channels

--- chapter5/socialpoll/twittervotes/twitter.go
+++ chapter5/socialpoll/twittervotes/twitter.go
@@ -136,3 +136,25 @@ func readFromTwitter(votes chan<- string) {
 		}
 	}
 }
+
+func startTwitterStream(stopchan <-chan struct{}, votes chan<- string) <-chan struct{} {
+	stoppedchan := make(chan struct{}, 1)
+	go func() {
+		defer func() {
+			stoppedchan <- struct{}{}
+		}()
+		for {
+			select {
+			case <-stopchan:
+				log.Println("Twitterへの問い合わせを終了します...")
+				return
+			default:
+				log.Println("Twitterに問い合わせます...")
+				readFromTwitter(votes)
+				log.Println(" (待機中)")
+				time.Sleep(10 * time.Second) // 待機してから再接続します
+			}
+		}
+	}()
+	return stoppedchan
+}
