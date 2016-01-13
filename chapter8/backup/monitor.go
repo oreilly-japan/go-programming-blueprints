a49088b7d748a943760c733916fc663decf3469d
Fumitoshi Ukai <fumitoshi.ukai@gmail.com>
2016-01-14 01:10:26 +0900

    chapter8 backup: Check for changes and initiate a backup (1)

---
+++ chapter8/backup/monitor.go
@@ -0,0 +1,26 @@
+package backup
+
+type Monitor struct {
+	Paths       map[string]string
+	Archiver    Archiver
+	Destination string
+}
+
+func (m *Monitor) Now() (int, error) {
+	var counter int
+	for path, lastHash := range m.Paths {
+		newHash, err := DirHash(path)
+		if err != nil {
+			return 0, err
+		}
+		if newHash != lastHash {
+			err := m.act(path)
+			if err != nil {
+				return counter, err
+			}
+			m.Paths[path] = newHash // ハッシュ値を更新します
+			counter++
+		}
+	}
+	return counter, nil
+}
